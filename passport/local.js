c46344187d33317956876bb97298ef1ca58a4977
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-03 19:19:35 -0700

    edits

--- passport/local.js
+++ passport/local.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { Strategy: LocalStrategy } = require('passport-local');
-const User = require('../models/user');
+const User = require('../models/User');
 
 const localStrategy = new LocalStrategy((username, password, done) => {
 
