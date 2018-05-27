35e5620a0d4f3bdee22383b759c6b7966a89d166
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-27 00:10:18 -0700

    deployment

--- config.js
+++ config.js
@@ -5,9 +5,7 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-//   DATABASE_URL:
-//         process.env.DATABASE_URL,
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'mongodb://localhost:27017'
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://infallible-goodall-e2cc46.netlify.com',
+  DATABASE_URL:
+        process.env.DATABASE_URL
 };
