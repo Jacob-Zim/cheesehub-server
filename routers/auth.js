4c383671768337026e20c6e845875aa25bf04c25
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-27 17:43:17 -0700

    error handler improvements for register

--- routers/auth.js
+++ routers/auth.js
@@ -88,9 +88,9 @@ router.post('/users', (req, res) => {
       code: 422,
       reason: 'ValidationError',
       message: tooSmallField
-        ? `Must be at least ${sizedFields[tooSmallField]
+        ? `Password must be at least ${sizedFields[tooSmallField]
           .min} characters long`
-        : `Must be at most ${sizedFields[tooLargeField]
+        : `Password must be at most ${sizedFields[tooLargeField]
           .max} characters long`,
       location: tooSmallField || tooLargeField
     });
