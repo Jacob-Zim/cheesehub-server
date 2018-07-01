3ea8415757a1e53a48833f12ee8d7ed49ce8efa1
JacobAZimmerman <ijacobzim@gmail.com>
2018-06-30 17:19:15 -0700

    edits to domain name

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
   PORT: process.env.PORT || 8080,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://infallible-goodall-e2cc46.netlify.com',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://skatespots.netlify.com',
   DATABASE_URL:
         process.env.DATABASE_URL
 };
